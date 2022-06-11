1aadb191ac914747b9ce05276bc0d62b68a923e8
Chris <chrismanfredijr@gmail.com>
2022-06-10 23:56:57 -0400

    API POST/GET routes

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
+const uniqid = require('uniqid');
+
 
 //port
 const PORT = process.env.PORT || 3001;
 
-//JSON
-const {notes} = require('./db/db.json');
 
 //middleware
 app.use(express.urlencoded({extended: true}));
@@ -16,16 +16,34 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
+//API ROUTING
+app.get("/api/notes", function (req, res) {
+    fs.readFileSync("./db/db.json", "utf8", function (err, data) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.json(data);
+    });
+  });
 
+
+app.post("/api/notes", (req, res) => {
+    var newNote = req.body;
+    var noteArray = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    newNote.id = uniqid();
+    noteArray.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(noteArray));
+})
+
+
+//HTML ROUTING 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
-
-
 
 //listener
 app.listen(PORT, () => {
